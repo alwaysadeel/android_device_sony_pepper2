8375c15cf2faadbcc44fadcfc34d3c9a2535d9cb
munjeni <sharecard17@gmail.com>
2013-10-20 22:10:55 +0200

    CM-10.2 for Xperia Sola

---
+++ config/cyttsp-spi.idc
@@ -0,0 +1,5 @@
+# Basic idc property file for the products
+
+touch.deviceType = touchScreen
+touch.distance.calibration = scaled
+glovemode.enabled = 1
---
+++ extract-files.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+VENDOR=sony
+DEVICE=pepper
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+
+while getopts ":nh" options
+do
+  case $options in
+    n ) NC=1 ;;
+    h ) echo "Usage: `basename $0` [OPTIONS] "
+        echo "  -n  No clenup"
+        echo "  -h  Show this help"
+        exit ;;
+    * ) ;;
+  esac
+done
+
+if [ "x$NC" != "x1" ];
+then
+    rm -rf $BASE/*
+fi
+
+for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    #adb pull /system/$FILE $BASE/$FILE
+    unzip -p /tmp/cm10.2_pepper-ota-eng.root_Build.1 system/$FILE > $BASE/$FILE
+done
+
+./setup-makefiles.sh
