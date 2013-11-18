a437d03ce359a95a8f18b97a4604724ed4335ac4
munjeni <sharecard17@gmail.com>
2013-11-18 15:20:54 +0100

    update

--- full_pepper.mk
+++ full_pepper.mk
@@ -30,7 +30,7 @@ PRODUCT_PACKAGES := \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 #charging animation
-$(call inherit-product, device/sony/pepper/prebuilt/resources-320x480.mk)
+$(call inherit-product, device/sony/pepper/prebuilt/resources-480x854.mk)
 
 # Inherit from pepper device
 $(call inherit-product, device/sony/pepper/pepper.mk)
